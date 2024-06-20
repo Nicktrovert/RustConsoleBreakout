313434612a72176ef1427b36055cc58e93f51c64
Nicktrovert <Praktikant@PRAKTIKANT21>
2024-06-20 11:11:15 +0200

    Yes.

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,13 @@ extern crate winapi;
 extern crate user32;
 extern crate kernel32;
 use std::*;
-use std::collections::HashSet;
-use std::future::poll_fn;
 use std::io::*;
-use std::ptr::read;
 use std::time::*;
-use rand::prelude::*;
 use crossterm::*;
-use crossterm::Result;
 use colored::Colorize;
-use crossterm::cursor::{MoveTo, position};
-use crossterm::event::{EnableMouseCapture, Event, KeyCode, poll};
+use crossterm::cursor::{MoveTo};
+use crossterm::event::{Event, KeyCode, poll};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use tokio::*;
 
 struct Obstacle{
     coords: (i16, i16),
@@ -26,12 +20,12 @@ impl Obstacle{
         return "████".to_string();
     }
 
-    fn new() -> Obstacle {
+    /*fn new() -> Obstacle {
         return Obstacle{
             coords: (0, 0),
             content: Obstacle::get_default_content().to_string(),
         }
-    }
+    }*/
 }
 
 fn clear_console(){
@@ -43,20 +37,20 @@ fn clear_console(){
     stdout.flush().unwrap();
 }
 
-fn InitializeNew_SetOf_Obstacles(rows: i32, columns: i32) -> Vec<Vec<Obstacle>> {
-    let mut obstacleList: Vec<Vec<Obstacle>> = Vec::new();
+fn initialize_new_set_of_obstacles(rows: i32, columns: i32) -> Vec<Vec<Obstacle>> {
+    let mut obstacle_list: Vec<Vec<Obstacle>> = Vec::new();
     for i in 1..rows{
-        let mut obstacleListElement: Vec<Obstacle> = Vec::new();
+        let mut obstacle_list_element: Vec<Obstacle> = Vec::new();
         for j in 1..columns / (Obstacle::get_default_content().to_string().chars().count() as i32) - 5{
             let new_obstacle = Obstacle{
                 coords: ((j * ((Obstacle::get_default_content().to_string().chars().count() as i32) + 1) - 3) as i16, i as i16),
                 content: Obstacle::get_default_content(),
             };
-            obstacleListElement.push(new_obstacle);
+            obstacle_list_element.push(new_obstacle);
         }
-        obstacleList.push(obstacleListElement);
+        obstacle_list.push(obstacle_list_element);
     }
-    return obstacleList;
+    return obstacle_list;
 }
 
 fn render_game(obstacles_list: &mut Vec<Vec<Obstacle>>){
@@ -72,14 +66,14 @@ fn render_obstacles(obstacles_list: &mut Vec<Vec<Obstacle>>, writer: &mut BufWri
     let color_vec = vec!["red", "green", "blue", "yellow", "magenta", "orange"];
     let mut y = 0;
     let mut color_iterator = 0;
-    for Obstacles in obstacles_list {
-        for obstacle in Obstacles{
-            if (obstacle.coords.1 != y){
+    for obstacles in obstacles_list {
+        for obstacle in obstacles {
+            if obstacle.coords.1 != y{
                 y = obstacle.coords.1;
                 write!(writer, "{}", crossterm::cursor::MoveTo(2, obstacle.coords.1 as u16)).expect("");
             }
             write!(writer, "{} ", obstacle.content.color(color_vec[color_iterator % color_vec.iter().count()])).expect("");
-            color_iterator += 1;
+            //color_iterator += 1;
         }
         color_iterator += 1;
     }
@@ -106,7 +100,7 @@ fn render_canvas(writer: &mut BufWriter<Stdout>) {
                         char_to_print = '═';
                     }
                 }
-                (h, w) | (h, w) if w == 0 || w == width - 1 => char_to_print = '║',
+                (_h, w) | (_h, w) if w == 0 || w == width - 1 => char_to_print = '║',
                 _ => {
                     if (i == 0 || i == height - 1) && (j != 0 && j != width - 1) {
                         char_to_print = '═';
@@ -125,26 +119,54 @@ fn render_canvas(writer: &mut BufWriter<Stdout>) {
 }
 
 
-fn print_events() -> String {
+fn print_events(obstacles_list: &mut Vec<Vec<Obstacle>>) -> String {
         if poll(Duration::from_millis(0)).expect("") {
             let event =  crossterm::event::read().unwrap();
 
-            write!(stdout(), "{}", MoveTo(terminal::size().unwrap().0 / 3, terminal::size().unwrap().1 - 2)).expect("");
+            write!(stdout(), "{}", MoveTo(terminal::size().unwrap().0 / 2, terminal::size().unwrap().1 - 2)).expect("");
             write!(stdout(), "Event::{:?}\r", event).expect("");
 
             if event == Event::Key(KeyCode::Esc.into()) {
                 return "esc".to_string();
             }
+            if event == Event::Key(KeyCode::Left.into()) || event == Event::Key(KeyCode::Char('a').into()){
+                for i in 0..obstacles_list.len(){
+                    if obstacles_list[i].len() == 0{
+                        continue;
+                    }
+                    else{
+                        obstacles_list[i].remove(0);
+                        break;
+                    }
+                }
+                return "left".to_string();
+            }
         } else {
-            // Timeout expired, no event for 1s
+            // Timeout expired, no event
         }
     return "".to_string();
+}
+
+async fn on_tick(obstacle_list: &mut Vec<Vec<Obstacle>>) -> Duration {
+    let time_now = std::time::Instant::now();
+    render_game(obstacle_list);
+    match print_events(obstacle_list).to_lowercase().as_str(){
+        "esc" => return Duration::from_secs(999),
+        "left" => {},
+        "right" => {},
+        _ => {}
+    }
+    write!(stdout(), "{}", MoveTo(0, 0)).expect("");
+    let elapsed_seconds = time_now.elapsed().as_secs_f64();
+    write!(stdout(), "{} seconds passed", elapsed_seconds).expect("");
+    stdout().flush().unwrap();
+    return time_now.elapsed();
 }
 
 #[tokio::main]
 async fn main() {
     enable_raw_mode().expect("");
-    let mut obstacleList: Vec<Vec<Obstacle>> = InitializeNew_SetOf_Obstacles(16, 113);
+    let mut obstacle_list: Vec<Vec<Obstacle>> = initialize_new_set_of_obstacles(16, 113);
     while terminal::size().unwrap() != (113, 31){
         stdout().execute(MoveTo(0, 0)).expect("");
         println!("Current Terminal Size: X={0};Y={1}", terminal::size().unwrap().0, terminal::size().unwrap().1);
@@ -162,8 +184,8 @@ async fn main() {
     let mut delay = tokio::time::interval(std::time::Duration::from_millis(&millis_per_second / refresh_rate));
     loop {
         delay.tick().await;
-        let time_to_run = on_tick(&mut obstacleList).await;
-        if (time_to_run.as_millis() == 0){
+        let time_to_run = on_tick(&mut obstacle_list).await;
+        if time_to_run.as_secs() == 999{
             break;
         }
         let calculated_delay_offset = time_to_run.as_secs() * 1000;
@@ -172,24 +194,8 @@ async fn main() {
 
     disable_raw_mode().expect("");
     clear_console();
-    stdout().execute(MoveTo(terminal::size().unwrap().0 / 3, terminal::size().unwrap().1 - 2)).expect("");
+    stdout().execute(MoveTo((terminal::size().unwrap().0 / 2) - "Press key to close...".len() as u16 / 2, terminal::size().unwrap().1 - 2)).expect("");
     write!(stdout(), "Press key to close...").expect("");
     stdout().flush().unwrap();
     let _ = crossterm::event::read().unwrap();
-}
-
-async fn on_tick(mut obstacleList: &mut Vec<Vec<Obstacle>>) -> Duration {
-        let time_now = std::time::Instant::now();
-        render_game(&mut obstacleList);
-        match (print_events().to_lowercase().as_str()){
-            "esc" => return Duration::from_millis(0),
-            "left" | "a" => return Duration::from_millis(0),
-            "right" | "d" => return Duration::from_millis(0),
-            _ => {}
-        }
-        write!(stdout(), "{}", MoveTo(0, 0)).expect("");
-        let elapsed_seconds = time_now.elapsed().as_secs_f64();
-        write!(stdout(), "{} seconds passed", elapsed_seconds).expect("");
-        stdout().flush().unwrap();
-        return time_now.elapsed();
 }
